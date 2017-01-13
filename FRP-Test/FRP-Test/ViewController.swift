71f3b60b7c403c687d492bcddf8ac2bd683066c7
Ryan King <ryanking@Ryans-iMac.local>
2017-01-13 15:28:36 +0000

    Test with ReactiveCocoa

--- FRP-Test/FRP-Test/ViewController.swift
+++ FRP-Test/FRP-Test/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import ReactiveCocoa
+import ReactiveSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet var usernameTextfield : UITextField!
+    @IBOutlet var passwordTextfield : UITextField!
+    @IBOutlet var signupButton : UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup the signup button initially
+        self.signupButton.isEnabled = false
+        signupButton.layer.cornerRadius = 4.0
+
+        self.usernameTextfield.reactive.continuousTextValues.observeValues { (string) in
+            if self.validTextFieldInput(input: string!) {
+                self.signupButton.isEnabled = true
+            } else {
+                self.signupButton.isEnabled = false
+            }
+        }
+        
+        self.passwordTextfield.reactive.continuousTextValues.observeValues { (string) in
+            if self.validTextFieldInput(input: string!) {
+                self.signupButton.isEnabled = true
+            } else {
+                self.signupButton.isEnabled = false
+            }
+        }
+    }
+    
+    func validTextFieldInput(input : String) -> Bool {
+        return input.characters.count > 4
     }
 
     override func didReceiveMemoryWarning() {
---
+++ FRP-Test/FRP-Test/ViewController2.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController2.swift
+//  FRP-Test
+//
+//  Created by Ryan King on 13/01/2017.
+//  Copyright © 2017 Ryan King. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+import ReactiveSwift
+
+class ViewController2: UIViewController {
+    
+    @IBOutlet var alphaPercentageLabel : UILabel!
+    @IBOutlet var radiusPercentageLabel : UILabel!
+    @IBOutlet var kittenImage : UIImageView!
+    @IBOutlet var alphaSlider : UISlider!
+    @IBOutlet var radiusStepper : UIStepper!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup the image initially
+        self.kittenImage.alpha = 0.0
+        self.kittenImage.layer.cornerRadius = 0.0
+        
+        self.alphaSlider.reactive.values.observeValues { (float) in
+            self.kittenImage.alpha = CGFloat(float)
+            self.alphaPercentageLabel.text = String(format: "%.00f", float * 100)
+        }
+        
+        self.radiusStepper.reactive.values.observeValues { (double) in
+            self.kittenImage.layer.cornerRadius = CGFloat(double)
+            self.radiusPercentageLabel.text = String(format: "%.00f", double)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
